a4b4e46b966a1e1418b73321ec65b1855f2d43ae
Brian Bansenauer <brianb@cascadia.edu>
2019-10-13 18:35:03 -0700

    intial

--- coredata-device/ViewController.swift
+++ coredata-device/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    var SerialNumbers:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Devices"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
-
+    @IBAction func addDevice(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "New Device", message: "Enter Device Serial Number", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default, handler: {
+            action in
+            guard let textField = alert.textFields?.first,
+                  let itemToSave = textField.text else
+            {
+                return
+            }
+            
+            self.SerialNumbers.append(itemToSave)
+            self.tableView.reloadData()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert,animated: true)
+    }
+    
 }
 
+//MARK _ TableView Data Source
+extension ViewController:UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = SerialNumbers[indexPath.row]
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return SerialNumbers.count
+    }
+}
